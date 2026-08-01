067ea7522dd4d0169307f34c7d6611e5f5e9ee92
Ahmed Alfy <ahmedalfy140@gmail.com>
2026-08-01 18:36:38 +0200

    navigate to home

--- lib/features/splash/presentaion/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentaion/views/widgets/splash_view_body.dart
@@ -1,8 +1,9 @@
-
-
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/features/home/presentation/views/home_view.dart';
 import 'package:bookly_app/features/splash/presentaion/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -11,43 +12,57 @@ class SplashViewBody extends StatefulWidget {
   State<SplashViewBody> createState() => _SplashViewBodyState();
 }
 
-class _SplashViewBodyState extends State<SplashViewBody>with SingleTickerProviderStateMixin {
-
-  late AnimationController animationController ;
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
   late Animation<Offset> slidingAnimation;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
-  
-    animationController=AnimationController(vsync: this,duration: Duration(seconds: 1));
-    slidingAnimation = Tween<Offset>(begin: Offset(0, 10),end: Offset(0, 0)).animate(animationController);
-    animationController.forward();
-  
-
+    navigateToHome();
+    initSlidingAnimation();
   }
+
   @override
-  void dispose(){
+  void dispose() {
     super.dispose();
     animationController.dispose();
-
-  
-
   }
 
   @override
   Widget build(BuildContext context) {
-    return  Column(
+    return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      
-      children:[ 
+
+      children: [
         Image.asset(AssetsData.logo),
-        SizedBox(height: 5,),
+        // SizedBox(height: 5,),
         slidingtext(slidingAnimation: slidingAnimation),
-         
-        
-        ]);
+      ],
+    );
+  }
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+    slidingAnimation = Tween<Offset>(
+      begin: Offset(0, 10),
+      end: Offset(0, 0),
+    ).animate(animationController);
+    animationController.forward();
+  }
+
+  void navigateToHome() {
+    Future.delayed(Duration(seconds: 3), () {
+      Get.to(
+        () => HomeView(),
+        transition: Transition.fade,
+        duration: kTransitionDuration,
+      );
+    });
   }
 }
-
