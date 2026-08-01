d2923dc1352b692d18d0cb9d86cf82fa85208e03
Ahmed Alfy <ahmedalfy140@gmail.com>
2026-08-01 14:38:53 +0200

    change themes and add text to splash view

--- lib/features/splash/presentaion/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentaion/views/widgets/splash_view_body.dart
@@ -6,6 +6,17 @@ class SplashViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Center(child: Image.asset(AssetsData.logo));
+    return  Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      
+      children:[ 
+        Image.asset(AssetsData.logo),
+        SizedBox(height: 5,),
+        Text(
+          'Reed Free Book', textAlign:TextAlign.center),
+         
+        
+        ]);
   }
 }
