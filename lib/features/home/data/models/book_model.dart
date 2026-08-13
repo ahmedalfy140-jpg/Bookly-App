240c5b07226fcd0ff072a111461e8755e6bd8092
Ahmed Alfy <ahmedalfy140@gmail.com>
2026-08-13 19:44:22 +0200

    create book model ,homerepo methode

---
+++ lib/features/home/data/models/book_model.dart
@@ -0,0 +1,35 @@
+class BookModel {
+  final String id;
+  final String title;
+  final String? author;
+  final String? description;
+  final String? image;
+  final double? rating;
+  final int? ratingCount;
+
+  BookModel({
+    required this.id,
+    required this.title,
+    this.author,
+    this.description,
+    this.image,
+    this.rating,
+    this.ratingCount,
+  });
+
+  factory BookModel.fromJson(json) {
+    return BookModel(
+      id: json['id'] ?? '',
+      title: json['volumeInfo']['title'] ?? 'No Title',
+      author: json['volumeInfo']['authors'] != null
+          ? json['volumeInfo']['authors'][0]
+          : 'Unknown',
+      description: json['volumeInfo']['description'] ?? '',
+      image: json['volumeInfo']['imageLinks'] != null
+          ? json['volumeInfo']['imageLinks']['thumbnail']
+          : null,
+      rating: json['volumeInfo']['averageRating']?.toDouble(),
+      ratingCount: json['volumeInfo']['ratingsCount'],
+    );
+  }
+}
